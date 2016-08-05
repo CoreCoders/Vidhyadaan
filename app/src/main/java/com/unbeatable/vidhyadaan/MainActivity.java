8365a0feae9161206c238869375965de166ccbdd
Rakshit <rakshit1993.rs@gmail.com>
2016-08-05 16:41:32 +0530

    Login Layout and Register Layout

--- app/src/main/java/com/unbeatable/vidhyadaan/MainActivity.java
+++ app/src/main/java/com/unbeatable/vidhyadaan/MainActivity.java
@@ -2,12 +2,27 @@ package com.unbeatable.vidhyadaan;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etEmailId, etPassword;
+    private Button btnSignIn, btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Init Component
+
+        etEmailId = (EditText) findViewById(R.id.et_emailId);
+        etPassword = (EditText) findViewById(R.id.et_password);
+
+        btnSignIn = (Button) findViewById(R.id.btn_signIn);
+        btnSignUp = (Button) findViewById(R.id.btn_signUp);
+
+        //Start from here
     }
 }
--- app/src/main/java/com/unbeatable/vidhyadaan/RegisterActivity.java
+++ app/src/main/java/com/unbeatable/vidhyadaan/RegisterActivity.java
@@ -2,12 +2,32 @@ package com.unbeatable.vidhyadaan;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatSpinner;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText etUserId, etFullName, etPassword, etRePassword;
+    private AppCompatSpinner spStd;
+    private Button btnSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        //Init Component
+
+        etUserId = (EditText) findViewById(R.id.et_userId);
+        etFullName = (EditText) findViewById(R.id.et_fullName);
+        etPassword = (EditText) findViewById(R.id.et_password_register);
+        etRePassword = (EditText) findViewById(R.id.et_reenterPassword);
+
+        spStd = (AppCompatSpinner) findViewById(R.id.sp_std);
+
+        btnSignup = (Button) findViewById(R.id.btnsignUp_register);
+
+        //Start from here
     }
 }
